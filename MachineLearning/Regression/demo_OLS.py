0882c61efc5fc49fc26723e18570b9660326b5cd
PC Lee <percycat.cis89@nctu.edu.tw>
2018-10-19 06:12:56 +0800

    Random samples follows normal distribution will be drawn and plot when the variance is changed

--- MachineLearning/Regression/demo_OLS.py
+++ MachineLearning/Regression/demo_OLS.py
@@ -14,13 +14,15 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
-t = np.arange(0.0, 10.0, 0.01)
 a0 = 1.5
-f0 = -7.5
-
+f0 = -2.5
+x_range = 5
+plt.axis([0, x_range, -10, 10])
+t = np.arange(0.0, x_range, 0.01)
 s = a0*t + f0
 l, = plt.plot(t, s, lw=2, color='red')
-plt.axis([0, 10, -10, 10])
+sample = np.random.normal(0, 1, len(t[0::10]))
+N=plt.scatter(t[0::10], s[0::10]+sample, color='green')
 
 axcolor = 'lightgoldenrodyellow'
 ax_var = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
@@ -28,10 +30,19 @@ ax_var = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 delta = 0.1
 sVar = Slider(ax_var, 'variance', 0.1, 5.0, valinit=1.0, valstep=delta)
 
-
-def update(val):
-    var = sVar.val
+def draw(var, color='red'):
+    ax.cla()
+    ax.axis([0, x_range, -10, 10])
+    ax.scatter(t[0::10], s[0::10]+sample, color='green')
+    ax.plot(t, s, lw=2, color=color)
     fig.canvas.draw_idle()
+    
+def update(val):
+    global sample
+    var = sVar.val
+    sample = np.random.normal(0, var, len(t[0::10]))
+    draw(var)
+    
 sVar.on_changed(update)
 
 resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
@@ -43,11 +54,11 @@ def reset(event):
 button.on_clicked(reset)
 
 rax = plt.axes([0.025, 0.5, 0.15, 0.15], facecolor=axcolor)
-radio = RadioButtons(rax, ('red', 'blue', 'green'), active=0)
+radio = RadioButtons(rax, ('red', 'blue', 'orange'), active=0)
 
 
 def colorfunc(label):
-    l.set_color(label)
+    draw(sVar.val, label)
     fig.canvas.draw_idle()
 radio.on_clicked(colorfunc)
 
