28756bb0f993ddff133ab6b817e39e0355f04278
PC Lee <percycat.cis89@nctu.edu.tw>
2018-10-31 06:16:56 +0800

    classification

---
+++ MachineLearning/Classification/Classification.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 06:59:54 2018
+
+@author: PCLee
+Demonstration of different classification methods
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+z = np.arange(-10, 10, 0.1)
+phi_z = sigmoid(z)
+
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+
+# y axis ticks and gridline
+plt.yticks([0.0, 0.5, 1.0])
+ax = plt.gca()
+ax.yaxis.grid(True)
+
+plt.tight_layout()
+# plt.savefig('./figures/sigmoid.png', dpi=300)
+plt.show()
