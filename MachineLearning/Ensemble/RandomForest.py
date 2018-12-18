7ac1f0b5b3937b91372e869f43b5d16a16017352
PC Lee <percycat.cis89@nctu.edu.tw>
2018-12-19 06:10:17 +0800

    add sklearn decision tree and adaboost

--- MachineLearning/Ensemble/RandomForest.py
+++ MachineLearning/Ensemble/RandomForest.py
@@ -7,6 +7,7 @@ Demo of scikit-learn random forest
 """
 
 import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
@@ -19,7 +20,14 @@ y = y - np.min(y)
 X = X.values  #covert to numpy array
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
-forest = RandomForestClassifier(criterion='entropy', n_estimators=20, random_state=1, n_jobs=2)
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+print( "score: {}".format( tree.score(X_test, y_test) ) )
 
+forest = RandomForestClassifier(criterion='entropy', n_estimators=20, random_state=1, n_jobs=2)
 forest.fit(X_train, y_train)
 print( "score: {}".format( forest.score(X_test, y_test) ) )
+
+AdaBosst = AdaBoostClassifier(base_estimator=forest, n_estimators=50, learning_rate=0.1, random_state=0)
+AdaBosst.fit(X_train, y_train)
+print( "score: {}".format( AdaBosst.score(X_test, y_test) ) )
